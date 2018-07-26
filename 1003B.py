12994e05e1faaebfd4033e13145d8f5e841a90ad
irazakharchenko <ira99.zakharchenko@gmail.com>
2018-07-26 15:31:50 +0200

    fix, path all tests

--- 1003B.py
+++ 1003B.py
@@ -1,8 +1,13 @@
 a, b, x = [int(s) for s in input().split()]
 n = a+b
-s = "0"
-a -= 1
-last_1 = False
+if a >= b:
+    s = "0"
+    a -= 1
+    last_1 = False
+else:
+    s = "1"
+    b -= 1
+    last_1 = True
 while len(s) < n:
     if x != 0:
         s += "1" if not last_1 else "0"
@@ -15,13 +20,14 @@ while len(s) < n:
     
     else:
         if a > 0:
-            s = "0" * a + s
+            find_0 = s.find("0")
+            s = s[:find_0] + "0" * a + s[find_0:]
             a = 0
         elif s[-1] == "1":
             s += b * "1"
             b = 0
         else:
-            s = s[:-1] + "1" * b + s[-1]
-    
-    
+            find_1 = s.find("1")
+            s = s[:find_1] + "1" * b + s[find_1:]
+            
 print(s)
